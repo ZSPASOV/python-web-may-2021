86dedfb21c10149d82f778c76f9a9eecbc3efb93
zspasov <zaprian_spasov@hotmail.com>
2021-05-30 21:52:37 +0300

    added django101 demos

---
+++ django101/django101/cities/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+from django101.cities.models import Person
+
+
+def index(req):
+    context = {
+        'name': 'Doncho',
+        'people': Person.objects.all(),
+    }
+
+    return render(req, 'index.html', context)
+
+
+def list_phones(request):
+    context = {
+        'phones': [
+            {
+                'name': 'Galaxy S5000',
+                'quantity': 3,
+            },
+            {
+                'name': 'Xiaomi Redmi 5',
+                'quantity': 0,
+            },
+            {
+                'name': 'iPhone 12',
+                'quantity': 4,
+            },
+        ]
+    }
+    # return HttpResponse('Phones list')'- taka beshe predi da e return render, ostavqm go za primer
+    context['message'] = 'Phones list'
+    return render(request, 'cities/phones.html', context)
